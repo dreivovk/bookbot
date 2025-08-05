4425175076bbb4e9ab35df3af9cec5a0b60559b3
dreivovk <drei.vovk@gmail.com>
2025-08-05 18:08:54 +0200

    added count words function

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
+def count_words(text):
+    return len(text.split())
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(text)
+    result_message = f"{num_words} words found in the document"
+    print(result_message)
 main()
