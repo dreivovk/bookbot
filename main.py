1fb501d3cb9c3bae8a83f8994037c5c56bc43ae1
dreivovk <drei.vovk@gmail.com>
2025-08-17 21:53:56 +0200

    added sort function and updated report

--- main.py
+++ main.py
@@ -3,13 +3,24 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
-from stats import count_words, count_characters
+from stats import count_words, count_chars, chars_dict_to_sorted_list
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
     num_words = count_words(text)
-    num_characters = count_characters(text)
-    result_message = f"{num_words} words found in the document"
-    print(result_message)
-    print(num_characters)
+    num_chars = count_chars(text)
+    sorted_chars = chars_dict_to_sorted_list(num_chars)
+    # print(f"{num_words} words found in the document")
+    # print(num_chars)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        if not item["char"].isalpha():
+            continue 
+        print(f"{item["char"]}: {item["num"]}")   
+    print("============= END ===============")     
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def count_words(text):
     return len(text.split())
 
-def count_characters(text):
+def count_chars(text):
     char_dictionary = {}
     for character in text:
         low_char = character.lower()
@@ -10,3 +10,14 @@ def count_characters(text):
         else:
             char_dictionary[low_char] = 1
     return char_dictionary
+
+def sort_on(items):
+    return items["num"]
+
+def chars_dict_to_sorted_list(char_dictionary):
+    dict_list = []
+    for char in char_dictionary:
+        number = char_dictionary[char]
+        dict_list.append({"char": char, "num": number})
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
